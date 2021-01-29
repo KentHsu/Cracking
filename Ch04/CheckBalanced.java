3c01abe774af662335189edbacbf814c70f0e5eb
luffy1610 <luffy1610@gmail.com>
2021-01-29 23:59:04 +0800

    Add Java code for Ch04

---
+++ Ch04/CheckBalanced.java
@@ -0,0 +1,16 @@
+import Tree
+
+public class CheckBalanced {
+
+	public static int treeHeight(Tree tree) {
+		
+	}
+
+	public static boolean checkBalanced(Tree tree) {
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
